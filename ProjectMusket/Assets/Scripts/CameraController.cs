58067e03aa1e319d569585242acbec402b3dd84d
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2020-03-16 20:26:55 +0100

    Character Controller added

---
+++ ProjectMusket/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offsetPos = new Vector3(0,10,-10);
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float turnSpeed = 10;
+    [SerializeField] float smoothSpeed = 0.5f;
+
+    Quaternion targetRotation;
+    Vector3 targetPos;
+    bool smoothRotating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithTarget();
+        LookAtTarget();
+    }
+
+    private void MoveWithTarget()
+    {
+        targetPos = target.position + offsetPos;
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    void LookAtTarget()
+    {
+        targetRotation = Quaternion.LookRotation(target.position - transform.position);
+    }
+}
---
+++ ProjectMusket/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float velocity = 5;
+    [SerializeField] private float turnSpeed = 10;
+
+    [SerializeField] private float sprintTimer = 1;
+    [SerializeField] private float sprintMultiplier = 2;
+
+    Vector2 input;
+    float angle;
+    Transform cam;
+    private Quaternion targetRotation;
+
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(this.transform.position, this.transform.position + new Vector3(5, 0, 0), Color.white, 0.5f);
+
+        GetInput();
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+
+
+}
